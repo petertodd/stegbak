f1d03833dd86ce3510e6456cd07c2bc571e6ebc1
Peter Todd <pete@petertodd.org>
2012-05-20 00:08:29 -0400

    Fixed bad comment

--- src/hide.c
+++ src/hide.c
@@ -1,5 +1,4 @@
-/* Finding data.
- * Copyright (C) 2012 Peter Todd
+/* Copyright (C) 2012 Peter Todd
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
